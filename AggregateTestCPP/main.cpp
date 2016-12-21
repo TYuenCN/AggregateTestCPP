d7348e596cb5fe79bc119c2fa73c6a288d9a738a
袁峥 <yuanzheng@yuanzhengdeMacBook-Pro.local>
2016-12-21 12:47:42 +0800

    Init

---
+++ AggregateTestCPP/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  AggregateTestCPP
+//
+//  Created by 袁峥 on 16/12/21.
+//  Copyright © 2016年 袁峥. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char **argv) {
+    //
+    //
+    // 启动参数
+    for (int i = 0; i != argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
+    
+    return 0;
+}
