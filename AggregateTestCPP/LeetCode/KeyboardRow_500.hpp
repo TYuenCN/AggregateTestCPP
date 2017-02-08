6fc478bf0548aeed73bcd0179115fed1d7238ca3
TsengYuen <tsengyuen@126.com>
2017-02-09 00:50:24 +0800

    ...

--- AggregateTestCPP/LeetCode/KeyboardRow_500.hpp
+++ AggregateTestCPP/LeetCode/KeyboardRow_500.hpp
@@ -15,6 +15,16 @@
 
 using namespace std;
 
+/**
+ * https://leetcode.com/problems/keyboard-row/
+ * Given a List of words, return the words that can be typed using 
+ * letters of alphabet on only one row's of American keyboard like 
+ * the image below.
+ *
+ * Example:
+ *      Input: ["Hello", "Alaska", "Dad", "Peace"]
+ *      Output: ["Alaska", "Dad"]
+ */
 class Solution500 {
 public:
     vector<string> findWords(vector<string>& words) {
